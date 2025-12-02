2c3602ace5afa79454d5353533044168b6738808
Ayllonvictor <ayllonvictor083@gmail.com>
2025-12-02 20:07:27 -0300

    Adicionar main.py esqueleto do sistema todo

---
+++ main.py
@@ -0,0 +1,46 @@
+tarefas = []
+
+# Membro 1
+def adicionar_tarefa():
+    pass
+
+# Membro 2
+def listar_tarefas():
+    pass
+
+# Membro 3
+def marcar_concluida():
+    pass
+
+# Membro 4
+def editar_tarefa():
+    pass
+
+# Membro 5
+def remover_tarefa():
+    pass
+
+# Membro 6
+def carregar_tarefas():
+    pass
+
+# Membro 7
+def salvar_tarefas():
+    pass
+
+def main():
+    while True:
+        print("===== SISTEMA TO-DO =====")
+        print("1 - Adicionar tarefa")
+        print("2 - Listar tarefas")
+        print("3 - Marcar como concluída")
+        print("4 - Editar tarefa")
+        print("5 - Remover tarefa")
+        print("6 - Sair")
+        escolha = input("Escolha: ")
+
+        if escolha == "6":
+            break
+
+if __name__ == "__main__":
+    main()
